93693c6b07dfe28e26318a92928fa673993e7a48
Spencer <rlthomas@brandeis.edu>
2018-06-13 15:02:52 -0400

    Added json shtuff and basic data storage

---
+++ modules/data.js
@@ -0,0 +1,6 @@
+{people:
+    [
+      {id:1, name:'Tim',githubId:"tjhickey724"},
+      {id:2, name:"ZZ", githubId:"abc123"}
+    ]
+}
--- routes/forms.js
+++ routes/forms.js
@@ -7,7 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+  const imdb = require('../node_modules/imdb-api');
   const movietitle = req.body.movietitle;
+  const movie = imdb.get(movietitle, {apiKey: '5530cdc3', timeout: 30000});
+  database.movies.push(movie)
+  fs.writeFileSync('../data.json',JSON.stringify(database,null,' '))
+  movies.push(movie)
   res.render('forms', { title: 'Forms', movietitle:movietitle });
 });
 
